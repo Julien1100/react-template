37ade5354fe896ffa7de677424e1d694f817a52f
J8T <jul.milcent@gmail.com>
2024-06-27 12:23:50 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Installation
+
+1. Clone this project
+2. Navigate to the `react-template` folder
+3. Run the command `npm i` or `npm install`
+4. Run the command `npm run dev` to start the program
---
+++ src/App.jsx
@@ -0,0 +1,118 @@
+function App() {
+  const source = [
+    {
+      nom: "Ingénierie informatique et intelligence artificielle",
+      description:
+        "Études centrées sur le développement de logiciels, la programmation et les applications de l'intelligence artificielle.",
+      duree_d_etude: "",
+      salaire: "35000-60000 EUR par an",
+      image_base64:
+        "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAwAB/ykNCHcAAAAASUVORK5CYII=",
+    },
+    {
+      nom: "Énergies renouvelables et développement durable",
+      description:
+        "Formation sur les technologies vertes, les énergies renouvelables et les stratégies de développement durable.",
+      duree_d_etude: "5 ans",
+      salaire: "",
+      image_base64: "",
+    },
+    {
+      nom: "Santé et biotechnologies",
+      description:
+        "Études sur les innovations médicales, les biotechnologies et les soins de santé.",
+      duree_d_etude: "5 à 8 ans",
+      salaire: "",
+      image_base64: "data:image/png;base64",
+    },
+    {
+      nom: "Art numérique et design graphique",
+      description: "",
+      duree_d_etude: "",
+      salaire: "25000-45000 EUR par an",
+      image_base64: "data:image/png;base64",
+    },
+    {
+      nom: "Commerce international et management",
+      description:
+        "Formation en gestion, commerce international et stratégie d'entreprise.",
+      duree_d_etude: "5 ans",
+      salaire: "",
+      image_base64: "data:image/png;base64",
+    },
+    {
+      nom: "Sciences de l'environnement et écologie",
+      description:
+        "Études sur la protection de l'environnement, la biodiversité et les sciences écologiques.",
+      duree_d_etude: "5 ans",
+      salaire: "30000-50000 EUR par an",
+      image_base64: "",
+    },
+    {
+      nom: "Droit et sciences politiques",
+      description: "",
+      duree_d_etude: "5 à 7 ans",
+      salaire: "35000-70000 EUR par an",
+      image_base64: "data:image/png;base64",
+    },
+    {
+      nom: "Mécatronique et robotique",
+      description:
+        "Études combinant la mécanique, l'électronique et l'informatique pour la conception de robots et de systèmes automatisés.",
+      duree_d_etude: "",
+      salaire: "35000-60000 EUR par an",
+      image_base64: "",
+    },
+    {
+      nom: "Psychologie et sciences cognitives",
+      description:
+        "Formation en psychologie, neuroscience et recherche cognitive.",
+      duree_d_etude: "",
+      salaire: "",
+      image_base64: "data:image/png;base64",
+    },
+    {
+      nom: "Tourisme et gestion hôtelière",
+      description:
+        "Études sur la gestion des entreprises touristiques et hôtelières.",
+      duree_d_etude: "3 à 5 ans",
+      salaire: "",
+      image_base64: "",
+    },
+  ];
+
+  return (
+    <>
+      <div class="container">
+        {source.map((data) => (
+          <div class="card">
+            <p class="name bold">{data.nom}</p>
+            <p key={data.name} class="description">
+              {data.description}
+            </p>
+            <div class="sub-info">
+              <div class="study-duration">
+                <p class="bold">Durée d'étude</p>
+                {data.duree_d_etude.length ? (
+                  <p>{data.duree_d_etude}</p>
+                ) : (
+                  <p class="italic">N/A</p>
+                )}
+              </div>
+              <div class="salary">
+                <p class="bold">Salaire</p>
+                {data.salaire.length ? (
+                  <p>{data.salaire}</p>
+                ) : (
+                  <p class="italic">N/A</p>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
